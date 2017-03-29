eb3496ec174f45d824f409272b8c0d860a1331c0
Yahya Gilany <yahya.gilany@live.com>
2017-03-29 00:53:13 -0400

    allows backticks and removed unused function

--- src/text-parser.ts
+++ src/text-parser.ts
@@ -1,11 +1,5 @@
 import { Range, Position, TextDocument } from 'vscode';
 
-export function isInString(text: string, character: number) : boolean {
-    let inSingleQuoationString = (text.substring(0, character).match(/\'/g) || []).length % 2 === 1;
-    let inDoubleQuoationString = (text.substring(0, character).match(/\"/g) || []).length % 2 === 1;
-    return inSingleQuoationString || inDoubleQuoationString;
-}
-
 export function isImportOrRequire(text) {
     let isImport = text.substring(0, 6) === 'import';
     let isRequire = text.indexOf('require(') != -1;
@@ -14,7 +8,7 @@ export function isImportOrRequire(text) {
 
 export function getTextWithinString(text: string, position: number) {
     const textToPosition = text.substring(0, position);
-    const quoatationPosition = Math.max(textToPosition.lastIndexOf('\"'), textToPosition.lastIndexOf('\''));
+    const quoatationPosition = Math.max(textToPosition.lastIndexOf('\"'), textToPosition.lastIndexOf('\''), textToPosition.lastIndexOf('\`'));
     return quoatationPosition != -1 ? textToPosition.substring(quoatationPosition + 1, textToPosition.length) : undefined;
 }
 
